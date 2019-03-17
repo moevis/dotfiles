669e224f2d419000e168e578a90645f049eb7a16
遇溪 <moevis.zjj@alibaba-inc.com>
2019-03-18 02:02:08 +0800

    add ncurses

---
+++ install_gtags.sh
@@ -0,0 +1,5 @@
+wget http://tamacom.com/global/global-6.6.3.tar.gz
+tar -xvzf global-6.6.3.tar.gz
+
+wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.1.tar.gz
+ln -s /work/lib/libncurses.so /work/lib/libcurses.so
