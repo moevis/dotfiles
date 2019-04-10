2e126f1dd1f0c8e0ae19f3486d328223bf3c8d30
Moevis <moevery@gmail.com>
2019-04-10 17:49:54 +0800

    Update and rename install_plugin.sh to install_nvim.sh

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,7 +1,8 @@
+# download nvim binary
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+# save to /usr/bin
+tar -xvzf nvim-linux64.tar.gz --strip 1 -C /usr
 
+# download vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-cd .config/nvim/plugged/YouCompleteMe/
-python install.py --clangd-completer
