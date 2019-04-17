7805025962b517fbd3f59848270b0a7945030680
Moevis <moevery@gmail.com>
2019-04-17 22:58:02 +0800

    Delete install_gtags.sh

--- install_gtags.sh
+++
@@ -1,5 +0,0 @@
-wget http://tamacom.com/global/global-6.6.3.tar.gz
-tar -xvzf global-6.6.3.tar.gz
-
-wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.1.tar.gz
-ln -s /work/lib/libncurses.so /work/lib/libcurses.so
