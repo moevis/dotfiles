1659e831458a82f96e7c8b8fbeb661870fe2f206
Moevis <moevery@gmail.com>
2019-04-17 20:25:27 +0800

    Update install_bash_it.sh

--- install_bash_it.sh
+++ install_bash_it.sh
@@ -1,4 +1,9 @@
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh --interactive
 
-# theme = brainy
+~/.bash_it/install.sh
+
+bash-it enable plugin git history
+
+# set theme = brainy
+sed -i 's/bobby/brainy/' ~/.bashrc
+
