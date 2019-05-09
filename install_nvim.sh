d14cc779cff4c4c47b9d938b6bb45c27314fdf7f
Moevis <moevery@gmail.com>
2019-05-09 13:19:23 +0800

    Update install_nvim.sh

--- install_nvim.sh
+++ install_nvim.sh
@@ -6,3 +6,6 @@ tar -xvzf nvim-linux64.tar.gz --strip 1 -C /usr
 # download vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
+# create init.vim
+curl -fLo ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/moevis/dotfiles/master/.nvimrc
