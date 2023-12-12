16733a38799bd2f03b6b0c01d1dbe5cd0f97afe2
Moevis <moevery@gmail.com>
2023-12-12 23:25:37 +0800

    reformat config, remove lsp_signature

--- init.lua
+++ init.lua
@@ -7,20 +7,15 @@ vim.cmd([[let g:go_def_mapping_enabled = 0]])
 vim.cmd([[let g:go_doc_keywordprg_enabled = 0 ]])
 
 vim.g.mapleader = ","
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 
 local o = vim.o
-local api = vim.api
-
 o.splitright = true
 o.showmatch = true
 o.termguicolors = true
 o.wildoptions = "pum"
 
-map("c", "<up>", 'pumvisible() ? "<C-p>" : "<up>"', { noremap = true, expr = true, silent = false })
-map("c", "<down>", 'pumvisible() ? "<C-n>" : "<down>"', { noremap = true, expr = true, silent = false })
-map("c", "<left>", 'pumvisible() ? "<up>" : "<left>"', { noremap = true, expr = true, silent = false })
-map("c", "<right>", 'pumvisible() ? "<C-e>" : "<right>"', { noremap = true, expr = true, silent = false })
+o.mouse = ""
 
 o.autoread = true
 o.hidden = true
@@ -45,7 +40,7 @@ o.autoindent = true
 o.ignorecase = true
 o.smartcase = true
 
-map("v", "<leader>o", ':OSCYank<cr>', { noremap = true })
+map("v", "<leader>o", ":OSCYank<cr>", { noremap = true })
 map("v", "<leader>y", '"+y<cr>', { noremap = true })
 map("v", "<leader>p", '"+p<cr>', { noremap = true })
 map("v", "<leader>x", '"_d<cr>', { noremap = true })
@@ -53,28 +48,30 @@ map("n", "gp", "`[v`]", {})
 map("n", "<leader>l", ":set invnumber<cr>", {})
 map("n", "<backspace>", ":nohl<cr>", { noremap = true })
 
--- telescope
 local opts = { noremap = true, silent = true }
+
+-- telescope
 
 map("n", "<leader>w", "<cmd>lua require('telescope.builtin').git_files({show_untracked = false})<CR>", opts)
 map("n", "<leader>s", "<cmd>lua require('telescope.builtin').git_status()<CR>", opts)
 map("n", "<leader>g", "<cmd>lua require('telescope.builtin').live_grep() <CR>", opts)
+map("n", "<leader>v", "<cmd>lua require('telescope.builtin').resume() <CR>", opts)
 map("n", "<leader>k", "<cmd>lua require('telescope.builtin').grep_string()<CR>", opts)
 
 map("n", "<leader>nn", "<cmd>NvimTreeToggle<CR>", opts)
 
 -- nvim-tree
 require("nvim-tree").setup({
-    diagnostics = {
-        enable = true,
-        icons = { hint = "", info = "", warning = "", error = "" }
-    },
+	diagnostics = {
+		enable = true,
+		icons = { hint = "", info = "", warning = "", error = "" },
+	},
 	git = { ignore = true },
 	filters = { custom = { ".git", "node_modules", ".cache" } },
-	renderer = { indent_markers = { enable = true }, },
+	renderer = { indent_markers = { enable = true } },
 	actions = {
 		open_file = {
-    	quit_on_open = true,
+			quit_on_open = true,
 			window_picker = {
 				enable = true,
 				exclude = {
@@ -102,36 +99,6 @@ require("lualine").setup({
 
 require("colorizer").setup({})
 
--- LSPConfig
-require("lsp_signature").setup()
-local nvim_lsp = require("lspconfig")
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
--- Lua language server config
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-nvim_lsp.sumneko_lua.setup({
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-				-- Setup your lua path
-				path = runtime_path,
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-		},
-	},
-})
-
-
 -- Set completeopt to have a better completion experience
 o.completeopt = "menu,menuone,noselect"
 -- o.completeopt = "menuone,noselect"
@@ -139,25 +106,87 @@ o.completeopt = "menu,menuone,noselect"
 -- nvim-cmp setup
 local cmp = require("cmp")
 cmp.setup({
-	mapping = {
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+		end,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-	},
-	sources = {
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
-		{ name = "buffer" },
-	},
+		{ name = "vsnip" }, -- For vsnip users.
+	}, { { name = "buffer" } }),
 })
 
--- Mappings.
-local opts = { noremap = true, silent = true }
+-- Set configuration for specific filetype.
+cmp.setup.filetype("gitcommit", {
+	sources = cmp.config.sources({
+		{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = "buffer" },
+	}),
+})
 
--- See `:help vim.lsp.*` for documentation on any of the below functions
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = { { name = "buffer" } },
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+})
+
+-- Set up lspconfig.
+require("mason").setup()
+require("mason-lspconfig").setup({ ensure_installed = { "gopls" } })
+
+-- LSPConfig
+require("lsp_signature").setup()
+local nvim_lsp = require("lspconfig")
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+nvim_lsp["gopls"].setup({})
+nvim_lsp["tsserver"].setup({})
+nvim_lsp["pyright"].setup({})
+nvim_lsp["clangd"].setup({ capabilities = capabilities, })
+
+-- Lua language server config
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+--
+-- nvim_lsp.sumneko_lua.setup({
+-- 	settings = {
+-- 		Lua = {
+-- 			runtime = {
+-- 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+-- 				version = "LuaJIT",
+-- 				-- Setup your lua path
+-- 				path = runtime_path,
+-- 			},
+-- 			diagnostics = { globals = { "vim" } },
+-- 			workspace = {
+-- 				library = vim.api.nvim_get_runtime_file("", true),
+-- 			},
+-- 		},
+-- 	},
+-- })
+
+-- Mappings.
 map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
 map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
@@ -166,26 +195,27 @@ map("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
 map("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 map("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-map("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+map("n", "<space>e", "<cmd>lua vim.diagnostic.open_float({scope='line'})<CR>", opts)
+map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()", opts)
+map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+map("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 
-map('n', 'gl', '<cmd>GoToFile<CR>', opts)
+map("n", "gl", "<cmd>GoToFile<CR>", opts)
 
 -- tmux Navigator
-require("Navigator").setup({})
+require("Navigator").setup({ disable_on_zoom = false })
 
-map("n", "<C-h>", "<CMD>lua require('Navigator').left()<CR>", opts)
-map("n", "<C-k>", "<CMD>lua require('Navigator').up()<CR>", opts)
-map("n", "<C-l>", "<CMD>lua require('Navigator').right()<CR>", opts)
-map("n", "<C-j>", "<CMD>lua require('Navigator').down()<CR>", opts)
-map("n", "<C-p>", "<CMD>lua require('Navigator').previous()<CR>", opts)
+-- map("n", "<C-h>", "<CMD>lua require('Navigator').left()<CR>", opts)
+-- map("n", "<C-k>", "<CMD>lua require('Navigator').up()<CR>", opts)
+-- map("n", "<C-l>", "<CMD>lua require('Navigator').right()<CR>", opts)
+-- map("n", "<C-j>", "<CMD>lua require('Navigator').down()<CR>", opts)
+-- map("n", "<C-p>", "<CMD>lua require('Navigator').previous()<CR>", opts)
+vim.keymap.set({ "n", "t" }, "<C-h>", "<CMD>NavigatorLeft<CR>")
+vim.keymap.set({ "n", "t" }, "<C-l>", "<CMD>NavigatorRight<CR>")
+vim.keymap.set({ "n", "t" }, "<C-k>", "<CMD>NavigatorUp<CR>")
+vim.keymap.set({ "n", "t" }, "<C-j>", "<CMD>NavigatorDown<CR>")
 
-require("Navigator").setup({
-	disable_on_zoom = false,
-})
-
-require("nvim-autopairs").setup({
-	map_cr = true,
-})
+require("nvim-autopairs").setup({ map_cr = true })
 
 local lspkind = require("lspkind")
 cmp.setup({
@@ -201,8 +231,8 @@ cmp.setup({
 		}),
 	},
 })
-vim.cmd([[hi CmpItemKind ctermfg=White]])
-vim.cmd([[hi CmpItemMenu ctermfg=White]])
+-- vim.cmd([[hi CmpItemKind ctermfg=White]])
+-- vim.cmd([[hi CmpItemMenu ctermfg=White]])
 
 require("trouble").setup({})
 
@@ -222,42 +252,40 @@ require("nvim-treesitter.configs").setup({
 	},
 })
 
-require "fidget".setup {}
+require("gitsigns").setup({
+	on_attach = function(bufnr)
+		local function map(mode, lhs, rhs, opts)
+			opts = vim.tbl_extend("force", { noremap = true, silent = true }, opts or {})
+			vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+		end
 
-require('gitsigns').setup{
-  on_attach = function(bufnr)
-    local function map(mode, lhs, rhs, opts)
-      opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
-      vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
-    end
+		-- Navigation
+		map("n", "]c", "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+		map("n", "[c", "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
 
-    -- Navigation
-    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
-    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+		-- Actions
+		map("n", "<leader>hs", ":Gitsigns stage_hunk<CR>")
+		map("v", "<leader>hs", ":Gitsigns stage_hunk<CR>")
+		map("n", "<leader>hr", ":Gitsigns reset_hunk<CR>")
+		map("v", "<leader>hr", ":Gitsigns reset_hunk<CR>")
+		map("n", "<leader>hS", "<cmd>Gitsigns stage_buffer<CR>")
+		map("n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>")
+		map("n", "<leader>hR", "<cmd>Gitsigns reset_buffer<CR>")
+		map("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>")
+		map("n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+		map("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
+		map("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>")
+		map("n", "<leader>hD", '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+		map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<CR>")
 
-    -- Actions
-    map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
-    map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
-    map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
-    map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
-    map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-    map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
-    map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
-    map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-    map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
-
-    -- Text object
-    map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-    map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  end
-}
+		-- Text object
+		map("o", "ih", ":<C-U>Gitsigns select_hunk<CR>")
+		map("x", "ih", ":<C-U>Gitsigns select_hunk<CR>")
+	end,
+})
 
 require("scrollbar").setup()
-require("transparent").setup({ enable = true })
+require("transparent").setup()
 require("indent_blankline").setup({ show_end_of_line = true })
 
 vim.notify = require("notify")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,61 +1,77 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
 end
 
-vim.cmd [[packadd packer.nvim cfilter]]
+vim.cmd([[packadd packer.nvim cfilter]])
 
-return require('packer').startup(function()
-  -- ui
-  use { "ellisonleao/gruvbox.nvim", requires = "rktjmp/lush.nvim" }
-  use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons' }
-  use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
-  use 'hoob3rt/lualine.nvim'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'numToStr/Navigator.nvim'
-  use 'folke/trouble.nvim'
-  use 'will133/vim-dirdiff'
-  use { 'lewis6991/gitsigns.nvim', requires = 'nvim-lua/plenary.nvim', }
-  use "rcarriga/nvim-notify"
-  use("petertriho/nvim-scrollbar")
-  use "xiyaowong/nvim-transparent"
-  use "RishabhRD/popfix"
-  use {"hood/popui.nvim", require={"RishabhRD/popfix"}}
-  use "lukas-reineke/indent-blankline.nvim"
+return require("packer").startup(function()
+	-- ui
+	use({ "ellisonleao/gruvbox.nvim", requires = "rktjmp/lush.nvim" })
+	use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+	use({ "nvim-telescope/telescope.nvim", requires = "nvim-lua/plenary.nvim" })
+	use("hoob3rt/lualine.nvim")
+	use("norcalli/nvim-colorizer.lua")
+	use({ "numToStr/Navigator.nvim" })
+	use("folke/trouble.nvim")
+	use("will133/vim-dirdiff")
+	use({ "lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim" })
+	use("rcarriga/nvim-notify")
+	use("petertriho/nvim-scrollbar")
+	use("xiyaowong/nvim-transparent")
+	use("RishabhRD/popfix")
+	use({ "hood/popui.nvim", require = { "RishabhRD/popfix" } })
+	use("lukas-reineke/indent-blankline.nvim")
+	use("jeffkreeftmeijer/vim-numbertoggle")
 
-  -- better code editing
-  use 'nvim-treesitter/nvim-treesitter'
-  use { "ThePrimeagen/refactoring.nvim", requires = "nvim-lua/plenary.nvim" }
-  use 'RRethy/nvim-treesitter-textsubjects'
-  use 'p00f/nvim-ts-rainbow'
-  use 'onsails/lspkind-nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/nvim-lsp-installer'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'ray-x/lsp_signature.nvim'
-  use 'windwp/nvim-autopairs'
-  use 'j-hui/fidget.nvim'
+	-- better code editing
+	use("nvim-treesitter/nvim-treesitter")
+	use({ "ThePrimeagen/refactoring.nvim", requires = "nvim-lua/plenary.nvim" })
+	use("RRethy/nvim-treesitter-textsubjects")
+	use("p00f/nvim-ts-rainbow")
+	use("onsails/lspkind-nvim")
+	use("neovim/nvim-lspconfig")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("windwp/nvim-autopairs")
 
-  -- toolset
-  use 'moevis/json.nvim'
-  use 'moevis/base64.nvim'
-  use 'moevis/smartjump.nvim'
-  use 'sbdchd/neoformat'
-  use 'tpope/vim-surround'
-  use 'numToStr/Comment.nvim'
-  use 'wellle/targets.vim'
-  use "sindrets/diffview.nvim"
-  use "NTBBloodbath/rest.nvim"
+	-- lsp
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/vim-vsnip")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("ray-x/lsp_signature.nvim")
 
+	-- toolset
+	use("moevis/json.nvim")
+	use("moevis/base64.nvim")
+	use("moevis/smartjump.nvim")
+	use("sbdchd/neoformat")
+	use("tpope/vim-surround")
+	use("numToStr/Comment.nvim")
+	use("wellle/targets.vim")
+	use("sindrets/diffview.nvim")
+	use("NTBBloodbath/rest.nvim")
+	use("github/copilot.vim")
 
-  -- language specific
-  use 'mtdl9/vim-log-highlighting'
-  use 'fatih/vim-go'
-  use "b0o/schemastore.nvim"
+	-- language specific
+	use("mtdl9/vim-log-highlighting")
+	use("fatih/vim-go")
+	use("b0o/schemastore.nvim")
 
-  -- copy via ssh
-  use 'ojroques/vim-oscyank'
+	-- copy via ssh
+	use("ojroques/vim-oscyank")
 end)
-
